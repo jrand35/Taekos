0c0c494b619008326e27a04e5be98806ada8a195
Joshua Rand <JRand@student.dwc.edu>
2015-04-07 14:05:49 -0400

    Added Game Over screen (prototype)

---
+++ Taekos/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	public Image gameOverImage;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Run ());
+	}
+
+	IEnumerator Run(){
+		for (float i = 0f, alpha = 0f; i <= 1f; i += 0.005f, alpha += 0.005f) {
+			float scale = 1f + 2f * (Mathf.Pow(i - 1, 2));
+			gameOverImage.rectTransform.localScale = new Vector3 (scale, scale, 1f);
+			gameOverImage.color = new Color(1f, 1f, 1f, alpha);
+			yield return 0;
+		}
+	}
+}
