63115d3ff7e6072a0bb61234d1c5bd84f390cfc0
Joshua Rand <JRand@student.dwc.edu>
2015-04-14 14:55:43 -0700

    Feathers now rotate instead of glow

---
+++ Taekos/Assets/Scripts/FeatherRotate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeatherRotate : MonoBehaviour {
+
+    private float dRad = 0.08f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Rotate());
+	}
+
+    IEnumerator Rotate()
+    {
+        float rad = 0f;
+        while (gameObject != null)
+        {
+            rad += dRad;
+            if (rad > (2f * Mathf.PI))
+            {
+                rad -= (2f * Mathf.PI);
+            }
+            float scale = Mathf.Cos(rad);
+            Vector3 newScale = transform.localScale;
+            newScale.x = scale;
+            transform.localScale = newScale;
+            yield return 0;
+        }
+    }
+}
