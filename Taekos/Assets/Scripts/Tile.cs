9936eceb09a992ec3719ab8b4237e55d7d8710ae
Joshua Rand <JRand@student.dwc.edu>
2015-04-14 11:57:51 -0700

    Updated .exe file

---
+++ Taekos/Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.renderer.material.mainTextureScale = new Vector2(0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
